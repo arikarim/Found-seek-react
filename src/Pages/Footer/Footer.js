0f1a8bad5779adf3d6cdf7024cfb5254dc5b04b6
arikarim <arikarim146@gmail.com>
2021-07-29 18:30:58 +0300

    Create footer

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="d-flex justify-content-between">
+      <div className="footer-item">Found&Seek</div>
+      <div className="footer-item">Copy right 2021</div>
+      <div className="footer-item">found&seek@gmail.com</div>
+    </div>
+  )
+}
+
+export default Footer
